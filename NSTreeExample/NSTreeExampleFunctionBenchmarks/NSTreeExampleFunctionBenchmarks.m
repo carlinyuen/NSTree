bac0f603fcc03fb526be6047a996171298ce7521
Carlin <email.me@carlinyuen.com>
2013-11-18 17:44:10 -0500

    deletion

--- NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
+++ NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
@@ -12,120 +12,186 @@
 
 @interface NSTreeExampleFunctionBenchmarks : XCTestCase
 
-    @property (nonatomic, strong) NSTree *tree3;
-    @property (nonatomic, strong) NSTree *tree30;
-    @property (nonatomic, strong) NSTree *tree300; 
-    
-    @property (nonatomic, strong) NSMutableArray *array10;
-    @property (nonatomic, strong) NSMutableArray *array1000; 
-    @property (nonatomic, strong) NSMutableArray *array1000000; 
-    
-    @property (nonatomic, strong) NSMutableDictionary *dict10;
-    @property (nonatomic, strong) NSMutableDictionary *dict1000; 
-    @property (nonatomic, strong) NSMutableDictionary *dict1000000; 
-       
-    @property (nonatomic, strong) NSMutableArray *data10;
-    @property (nonatomic, strong) NSMutableArray *data1000; 
-    @property (nonatomic, strong) NSMutableArray *data1000000;  
+//    @property (nonatomic, strong) NSTree *tree3;
+//    @property (nonatomic, strong) NSTree *tree30;
+//    @property (nonatomic, strong) NSTree *tree300; 
+//    
+//    @property (nonatomic, strong) NSMutableArray *array10;
+//    @property (nonatomic, strong) NSMutableArray *array1000; 
+//    @property (nonatomic, strong) NSMutableArray *array1000000; 
+//    
+//    @property (nonatomic, strong) NSMutableDictionary *dict10;
+//    @property (nonatomic, strong) NSMutableDictionary *dict1000; 
+//    @property (nonatomic, strong) NSMutableDictionary *dict1000000; 
+//       
+//    @property (nonatomic, strong) NSMutableArray *data10;
+//    @property (nonatomic, strong) NSMutableArray *data1000; 
+//    @property (nonatomic, strong) NSMutableArray *data1000000;  
     
 @end
 
+static NSTree *tree3;
+static NSTree *tree30;
+static NSTree *tree300; 
+
+static NSMutableArray *array10;
+static NSMutableArray *array1000; 
+static NSMutableArray *array1000000; 
+
+static NSMutableDictionary *dict10;
+static NSMutableDictionary *dict1000; 
+static NSMutableDictionary *dict1000000; 
+   
+static NSMutableArray *data10;
+static NSMutableArray *data1000; 
+static NSMutableArray *data1000000;  
+
 @implementation NSTreeExampleFunctionBenchmarks
 
-- (void)setUp
++ (void)setUp
 {
     [super setUp];
      
     // Setup data
-    self.data10 = [NSMutableArray new];
-    self.data1000 = [NSMutableArray new]; 
-    self.data1000000 = [NSMutableArray new]; 
+    data10 = [NSMutableArray new];
+    data1000 = [NSMutableArray new]; 
+    data1000000 = [NSMutableArray new]; 
     for (int i = 1; i <= 1000000; ++i) {
-        if (i <= 10) {[self.data10 addObject: @(i)];}
-        if (i <= 1000) {[self.data1000 addObject: @(i)];} 
-        [self.data1000000 addObject: @(i)];
+        if (i <= 10) {[data10 addObject: @(i)];}
+        if (i <= 1000) {[data1000 addObject: @(i)];} 
+        [data1000000 addObject: @(i)];
     }   
 
     // Trees
-    self.tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:self.data10];   
-    self.tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:self.data1000];  
-    self.tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:self.data1000000];  
+    tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:data10];   
+    tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:data1000];  
+    tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:data1000000];  
     
     // Arrays
-    self.array10 = [[NSMutableArray alloc] initWithArray:self.data10];
-    self.array1000 = [[NSMutableArray alloc] initWithArray:self.data1000]; 
-    self.array1000000 = [[NSMutableArray alloc] initWithArray:self.data1000000];
+    array10 = [[NSMutableArray alloc] initWithArray:data10];
+    array1000 = [[NSMutableArray alloc] initWithArray:data1000]; 
+    array1000000 = [[NSMutableArray alloc] initWithArray:data1000000];
     
     // Dictionaries
-    self.dict10 = [NSMutableDictionary new];  
-    for (id object in self.data10) {
-        [self.dict10 setObject:object forKey:[object description]];
+    dict10 = [NSMutableDictionary new];  
+    for (id object in data10) {
+        [dict10 setObject:object forKey:[object description]];
     }
-    self.dict1000 = [NSMutableDictionary new];  
-    for (id object in self.data1000) {
-        [self.dict1000 setObject:object forKey:[object description]];
+    dict1000 = [NSMutableDictionary new];  
+    for (id object in data1000) {
+        [dict1000 setObject:object forKey:[object description]];
     } 
-    self.dict1000000 = [NSMutableDictionary new]; 
-    for (id object in self.data1000000) {
-        [self.dict1000000 setObject:object forKey:[object description]];
+    dict1000000 = [NSMutableDictionary new]; 
+    for (id object in data1000000) {
+        [dict1000000 setObject:object forKey:[object description]];
     } 
 }
 
-- (void)tearDown
++ (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
+#pragma mark - Insertion
+
 - (void)testInsertTree3 {
-    id object = @(self.data10.count / 2.0);
-    [self.tree3 addObject:object];
+    id object = @(data10.count / 2.0);
+    [tree3 addObject:object];
 }
 
 - (void)testInsertArray10 {
-    id object = @(self.data10.count / 2.0);
-    [self.array10 insertObject:object atIndex:[self.array10 indexOfObject:object inSortedRange:NSMakeRange(0, self.array10.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
+    id object = @(data10.count / 2.0);
+    [array10 insertObject:object atIndex:[array10 indexOfObject:object inSortedRange:NSMakeRange(0, array10.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
         return [obj1 compare:obj2];
     }]];
 }
 
 - (void)testInsertDict10 {
-    id object = @(self.data10.count / 2.0);
-    [self.dict10 setObject:object forKey:[object description]];
+    id object = @(data10.count / 2.0);
+    [dict10 setObject:object forKey:[object description]];
 }
 
 - (void)testInsertTree30 {
-    id object = @(self.data1000.count / 2.0);
-    [self.tree30 addObject:object];
+    id object = @(data1000.count / 2.0);
+    [tree30 addObject:object];
 }
 
 - (void)testInsertArray1000 {
-    id object = @(self.data1000.count / 2.0);
-    [self.array1000 insertObject:object atIndex:[self.array1000 indexOfObject:object inSortedRange:NSMakeRange(0, self.array1000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
+    id object = @(data1000.count / 2.0);
+    [array1000 insertObject:object atIndex:[array1000 indexOfObject:object inSortedRange:NSMakeRange(0, array1000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
         return [obj1 compare:obj2];
     }]];
 }
 
 - (void)testInsertDict1000 {
-    id object = @(self.data1000.count / 2.0);
-    [self.dict1000 setObject:object forKey:[object description]];
+    id object = @(data1000.count / 2.0);
+    [dict1000 setObject:object forKey:[object description]];
 }
 
 - (void)testInsertTree300 {
-    id object = @(self.data1000000.count / 2.0);
-    [self.dict1000000 setObject:object forKey:[object description]];
+    id object = @(data1000000.count / 2.0);
+    [tree300 addObject:object];
 }
 
 - (void)testInsertArray1000000 {
-    id object = @(self.data1000000.count / 2.0);
-    [self.array1000000 insertObject:object atIndex:[self.array1000000 indexOfObject:object inSortedRange:NSMakeRange(0, self.array1000000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
+    id object = @(data1000000.count / 2.0);
+    [array1000000 insertObject:object atIndex:[array1000000 indexOfObject:object inSortedRange:NSMakeRange(0, array1000000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
         return [obj1 compare:obj2];
     }]];
 }
 
 - (void)testInsertDict1000000 {
-    id object = @(self.data1000000.count / 2.0);
-    [self.dict1000000 setObject:object forKey:[object description]];
+    id object = @(data1000000.count / 2.0);
+    [dict1000000 setObject:object forKey:[object description]];
+}
+
+
+#pragma mark - Deletion
+
+- (void)testDeleteTree3 {
+    id object = @(data10.count / 2.0);
+    [tree3 removeObject:object];
+}
+
+- (void)testDeleteArray10 {
+    id object = @(data10.count / 2.0);
+    [array10 removeObject:object];
+}
+
+- (void)testDeleteDict10 {
+    id object = @(data10.count / 2.0);
+    [dict10 removeObjectForKey:[object description]];
+}
+
+- (void)testDeleteTree30 {
+    id object = @(data1000.count / 2.0);
+    [tree30 removeObject:object];
+}
+
+- (void)testDeleteArray1000 {
+    id object = @(data1000.count / 2.0);
+    [array1000 removeObject:object];
+}
+
+- (void)testDeleteDict1000 {
+    id object = @(data1000.count / 2.0);
+    [dict1000 removeObjectForKey:[object description]]; 
+}
+
+- (void)testDeleteTree300 {
+    id object = @(data1000000.count / 2.0);
+    [tree300 removeObject:object];
+}
+
+- (void)testDeleteArray1000000 {
+    id object = @(data1000000.count / 2.0);
+    [array1000000 removeObject:object];
+}
+
+- (void)testDeleteDict1000000 {
+    id object = @(data1000000.count / 2.0);
+    [dict1000000 removeObjectForKey:[object description]];  
 }
 
 @end
