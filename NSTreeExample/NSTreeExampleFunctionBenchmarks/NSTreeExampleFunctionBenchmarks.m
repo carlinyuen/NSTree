887af2d0b76f456fc49db651596b04d0f944feba
Carlin <email.me@carlinyuen.com>
2013-12-18 12:49:05 -0500

    cleaning up unit test and adding core data benchmarking

--- NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
+++ NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
@@ -13,39 +13,23 @@
 
 #import "NSTree.h"
 
-@interface NSTreeExampleFunctionBenchmarks : XCTestCase
+    #define NUM_ELEMENTS 1000000
 
+@interface NSTreeExampleFunctionBenchmarks : XCTestCase
 @end
 
 static NSTree *tree3;
 static NSTree *tree30;
 static NSTree *tree300; 
+static NSMutableArray *array; 
+static NSMutableDictionary *dict; 
+static NSMutableArray *data; 
+static NSMutableArray *searchCriteria;
+static NSMutableArray *insertCriteria;
+static NSMutableArray *deleteCriteria;
 
-static NSMutableArray *array10;
-static NSMutableArray *array1000; 
-static NSMutableArray *array1000000; 
-
-static NSMutableDictionary *dict10;
-static NSMutableDictionary *dict1000; 
-static NSMutableDictionary *dict1000000; 
-   
-static NSMutableArray *data10;
-static NSMutableArray *data1000; 
-static NSMutableArray *data1000000; 
-
-static NSMutableArray *searchCriteria10;
-static NSMutableArray *searchCriteria1000;
-static NSMutableArray *searchCriteria1000000;
-
-static NSMutableArray *insertCriteria10;
-static NSMutableArray *insertCriteria1000;
-static NSMutableArray *insertCriteria1000000;
-
-static NSMutableArray *deleteCriteria10;
-static NSMutableArray *deleteCriteria1000;
-static NSMutableArray *deleteCriteria1000000;
-
-static NSManagedObjectContext *context;
+static NSManagedObjectModel *mom;
+static NSManagedObjectContext *moc;
 
 @implementation NSTreeExampleFunctionBenchmarks
 
@@ -54,83 +38,71 @@ static NSManagedObjectContext *context;
     [super setUp];
      
     // Setup data
-    data10 = [NSMutableArray new];
-    data1000 = [NSMutableArray new]; 
-    data1000000 = [NSMutableArray new]; 
-    for (int i = 1; i <= 1000000; ++i) {
-        if (i <= 10) {[data10 addObject: @(i)];}
-        if (i <= 1000) {[data1000 addObject: @(i)];} 
-        [data1000000 addObject: @(i)];
+    data = [NSMutableArray new]; 
+    for (int i = 1; i <= NUM_ELEMENTS; ++i) {
+        [data addObject: @(i)];
     }
     
     // Criteria
-    searchCriteria10 = [NSMutableArray new];
-    searchCriteria1000 = [NSMutableArray new]; 
-    searchCriteria1000000 = [NSMutableArray new];  
-    insertCriteria10 = [NSMutableArray new];
-    insertCriteria1000 = [NSMutableArray new]; 
-    insertCriteria1000000 = [NSMutableArray new];  
-    deleteCriteria10 = [NSMutableArray new];
-    deleteCriteria1000 = [NSMutableArray new]; 
-    deleteCriteria1000000 = [NSMutableArray new]; 
-    for (int i = 0; i < 1000; ++i) {
-        if (i < data10.count / 2) {
-            [searchCriteria10 addObject:@(arc4random() % data10.count)];
-            [insertCriteria10 addObject:@(arc4random() % data10.count)]; 
-            [deleteCriteria10 addObject:@(arc4random() % data10.count)]; 
-        }
-        if (i < data1000.count / 2) {
-            [searchCriteria1000 addObject:@(arc4random() % data1000.count)];
-            [insertCriteria1000 addObject:@(arc4random() % data1000.count)]; 
-            [deleteCriteria1000 addObject:@(arc4random() % data1000.count)];  
-        }
-       [searchCriteria1000000 addObject:@(arc4random() % data1000000.count)];
-       [insertCriteria1000000 addObject:@(arc4random() % data1000000.count)];  
-       [deleteCriteria1000000 addObject:@(arc4random() % data1000000.count)];   
+    searchCriteria = [NSMutableArray new];  
+    insertCriteria = [NSMutableArray new];  
+    deleteCriteria = [NSMutableArray new]; 
+    for (int i = 0; i < NUM_ELEMENTS / 1000; ++i) {
+       [searchCriteria addObject:@(arc4random() % data.count)];
+       [insertCriteria addObject:@(arc4random() % data.count)];  
+       [deleteCriteria addObject:@(arc4random() % data.count)];   
     }
 
-    // Trees
-    tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:data10];   
-    tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:data1000];  
-    tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:data1000000];  
+    // Data Structures
+    tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:data];   
+    tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:data];  
+    tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:data];  
+    array = [[NSMutableArray alloc] initWithArray:data];
+    dict = [NSMutableDictionary new]; 
+    for (id object in data) {
+        [dict setObject:object forKey:[object description]];
+    } 
     
-    // Arrays
-    array10 = [[NSMutableArray alloc] initWithArray:data10];
-    array1000 = [[NSMutableArray alloc] initWithArray:data1000]; 
-    array1000000 = [[NSMutableArray alloc] initWithArray:data1000000];
+    // Setup CoreData Entity - simple entry:value
+    NSEntityDescription *runEntity = [[NSEntityDescription alloc] init];
+    [runEntity setName:@"Entry"];
+    [runEntity setManagedObjectClassName:@"Entry"];
+     
+    NSAttributeDescription *attr= [[NSAttributeDescription alloc] init];
+    [attr setName:@"value"];
+    [attr setAttributeType:NSInteger32AttributeType];
+    [attr setOptional:NO];
+    [attr setDefaultValue:@0];
+    [runEntity setProperties:@[attr]];
     
-    // Dictionaries
-    dict10 = [NSMutableDictionary new];  
-    for (id object in data10) {
-        [dict10 setObject:object forKey:[object description]];
+    // Managed Object Model
+    mom = [[NSManagedObjectModel alloc] init]; 
+    [mom setEntities:@[runEntity]]; 
+    
+    // Persistent Store - use in-memory
+    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
+    NSError *error = nil;
+    [psc addPersistentStoreWithType:NSInMemoryStoreType
+        configuration:nil URL:nil options:nil error:&error];
+    if (error) {
+        NSLog(@"Error creating persistent store: %@", error);
     }
-    dict1000 = [NSMutableDictionary new];  
-    for (id object in data1000) {
-        [dict1000 setObject:object forKey:[object description]];
-    } 
-    dict1000000 = [NSMutableDictionary new]; 
-    for (id object in data1000000) {
-        [dict1000000 setObject:object forKey:[object description]];
-    } 
     
-    // Setup CoreData
-//    NSArray *bundles = [NSArray arrayWithObject:[NSBundle bundleForClass:[self class]]];
-//    NSManagedObjectModel *mom = [NSManagedObjectModel mergedModelFromBundles:bundles];
-//    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
-//    context = [[NSManagedObjectContext alloc] init];
-//    context.persistentStoreCoordinator = psc;
-//    
-//    NSManagedObject *mo;
-//    for (id object in data1000000) {
-//        mo = [NSEntityDescription 
-//              insertNewObjectForEntityForName:@"Entity" 
-//              inManagedObjectContext:context]; 
-//        [mo setValue:object forKey:@"value"]; 
-//    }
-//    NSError *error = nil;
-//    if (![context save:&error]) {
-//        NSLog(@"Populating CoreData Failed");
-//    }
+    // Managed Object Context
+    moc = [[NSManagedObjectContext alloc] init];
+    moc.persistentStoreCoordinator = psc;
+
+    NSManagedObject *mo;
+    for (id object in data) {
+        mo = [NSEntityDescription 
+            insertNewObjectForEntityForName:@"Entry" 
+            inManagedObjectContext:moc]; 
+        [mo setValue:object forKey:@"value"]; 
+    }
+    error = nil;
+    if (![moc save:&error]) {
+        NSLog(@"Populating CoreData Failed");
+    }
 }
 
 + (void)tearDown
@@ -142,66 +114,38 @@ static NSManagedObjectContext *context;
 #pragma mark - Insertion
 
 - (void)testInsertTree3 {
-    for (id object in insertCriteria10) {
+    for (id object in insertCriteria) {
         [tree3 addObject:object];
     }
 }
 
-- (void)testInsertArray10 {
-    for (id object in insertCriteria10) {
-        [array10 insertObject:object atIndex:[array10 indexOfObject:object inSortedRange:NSMakeRange(0, array10.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }]];
-    }
-}
-
-- (void)testInsertDict10 {
-    for (id object in insertCriteria10) {
-        [dict10 setObject:object forKey:[object description]];
-    }
-}
-
 - (void)testInsertTree30 {
-    for (id object in insertCriteria1000) {
+    for (id object in insertCriteria) {
         [tree30 addObject:object];
     }
 }
 
-- (void)testInsertArray1000 {
-    for (id object in insertCriteria1000) {
-        [array1000 insertObject:object atIndex:[array1000 indexOfObject:object inSortedRange:NSMakeRange(0, array1000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
-            return [obj1 compare:obj2];
-        }]];
-    }
-}
-
-- (void)testInsertDict1000 {
-    for (id object in insertCriteria1000) {
-        [dict1000 setObject:object forKey:[object description]];
-    }
-}
-
 - (void)testInsertTree300 {
-    for (id object in insertCriteria1000000) {
+    for (id object in insertCriteria) {
         [tree300 addObject:object];
     }
 }
 
-- (void)testInsertArray1000000 {
-    for (id object in insertCriteria1000000) {
-        [array1000000 insertObject:object atIndex:[array1000000 indexOfObject:object inSortedRange:NSMakeRange(0, array1000000.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
+- (void)testInsertArray {
+    for (id object in insertCriteria) {
+        [array insertObject:object atIndex:[array indexOfObject:object inSortedRange:NSMakeRange(0, array.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
             return [obj1 compare:obj2];
         }]];
     }
 }
 
-- (void)testInsertDict1000000 {
-    for (id object in insertCriteria1000000) {
-        [dict1000000 setObject:object forKey:[object description]];
+- (void)testInsertDict {
+    for (id object in insertCriteria) {
+        [dict setObject:object forKey:[object description]];
     }
 }
 
-- (void)testInsertCoreData1000000 {
+- (void)testInsertCoreData {
 //    NSManagedObject *mo;
 //    for (id object in insertCriteria1000000) {
 //        mo = [NSEntityDescription 
@@ -227,62 +171,38 @@ static NSManagedObjectContext *context;
 #pragma mark - Deletion
 
 - (void)testDeleteTree3 {
-    for (id object in deleteCriteria10) {
+    for (id object in deleteCriteria) {
         [tree3 removeObject:object];
     }
 }
 
-- (void)testDeleteArray10 {
-    for (id object in deleteCriteria10) {
-        [array10 removeObject:object];
-    }
-}
-
-- (void)testDeleteDict10 {
-    for (id object in deleteCriteria10) {
-        [dict10 removeObjectForKey:[object description]];
-    }
-}
-
 - (void)testDeleteTree30 {
-    for (id object in deleteCriteria1000) {
+    for (id object in deleteCriteria) {
         [tree30 removeObject:object];
     }
 }
 
-- (void)testDeleteArray1000 {
-    for (id object in deleteCriteria1000) {
-        [array1000 removeObject:object];
-    }
-}
-
-- (void)testDeleteDict1000 {
-    for (id object in deleteCriteria1000) {
-        [dict1000 removeObjectForKey:[object description]]; 
-    }
-}
-
 - (void)testDeleteTree300 {
-    for (id object in deleteCriteria1000000) {
+    for (id object in deleteCriteria) {
         [tree300 removeObject:object];
     }
 }
 
-- (void)testDeleteArray1000000 {
-    for (id object in deleteCriteria1000000) {
-        [array1000000 removeObject:object];
+- (void)testDeleteArray {
+    for (id object in deleteCriteria) {
+        [array removeObject:object];
     }
 }
 
-- (void)testDeleteDict1000000 {
-    for (id object in deleteCriteria1000000) {
-        [dict1000000 removeObjectForKey:[object description]];  
+- (void)testDeleteDict {
+    for (id object in deleteCriteria) {
+        [dict removeObjectForKey:[object description]];  
     }
 }
 
-- (void)testDeleteCoreData1000000 {
+- (void)testDeleteCoreData {
 //    NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:@"Entity"]; 
-//    NSPredicate *p = [NSPredicate predicateWithFormat:@"value == %@", [deleteCriteria1000000 componentsJoinedByString:@" OR value == "]]; 
+//    NSPredicate *p = [NSPredicate predicateWithFormat:@"value == %@", [deleteCriteria componentsJoinedByString:@" OR value == "]]; 
 //    [fetch setPredicate:p]; 
 //    NSError *error;
 //    NSArray *results = [context executeFetchRequest:fetch error:&error]; 
@@ -305,56 +225,32 @@ static NSManagedObjectContext *context;
 #pragma mark - Search
 
 - (void)testSearchTree3 {
-    for (id object in searchCriteria10) {
+    for (id object in searchCriteria) {
         [tree3 containsObject:object];
     }
 }
 
-- (void)testSearchArray10 {
-    for (id object in searchCriteria10) {
-        [array10 containsObject:object];
-    }
-}
-
-- (void)testSearchDict10 {
-    for (id object in searchCriteria10) {
-        [dict10 objectForKey:[object description]];
-    }
-}
-
 - (void)testSearchTree30 {
-    for (id object in searchCriteria1000) {
+    for (id object in searchCriteria) {
         [tree30 containsObject:object];
     }
 }
 
-- (void)testSearchArray1000 {
-    for (id object in searchCriteria1000) {
-        [array1000 containsObject:object];
-    }
-}
-
-- (void)testSearchDict1000 {
-    for (id object in searchCriteria1000) {
-        [dict1000 objectForKey:[object description]]; 
-    }
-}
-
 - (void)testSearchTree300 {
-    for (id object in searchCriteria1000000) {
+    for (id object in searchCriteria) {
         [tree300 containsObject:object];
     }
 }
 
-- (void)testSearchArray1000000 {
-    for (id object in searchCriteria1000000) {
-        [array1000000 containsObject:object];
+- (void)testSearchArray {
+    for (id object in searchCriteria) {
+        [array containsObject:object];
     }
 }
 
-- (void)testSearchDict1000000 {
-    for (id object in searchCriteria1000000) {
-        [dict1000000 objectForKey:[object description]];  
+- (void)testSearchDict {
+    for (id object in searchCriteria) {
+        [dict objectForKey:[object description]];  
     }
 }
 
