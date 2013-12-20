e9d9e74aa52e0896f254169b65825ba52f7516dc
Carlin <email.me@carlinyuen.com>
2013-12-20 05:39:43 -0500

    trying to test sqlite persistence for core data

--- NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
+++ NSTreeExample/NSTreeExampleFunctionBenchmarks/NSTreeExampleFunctionBenchmarks.m
@@ -16,6 +16,11 @@
     #define NUM_ELEMENTS 1000000    // This is extremely slow on CoreData functions
 //    #define NUM_ELEMENTS 1000
 
+    #define TREE 0
+    #define ARRAY 0
+    #define DICT 0
+    #define CORE 1
+
 @interface NSTreeExampleFunctionBenchmarks : XCTestCase
 @end
 
@@ -56,64 +61,76 @@ static NSManagedObjectContext *moc;
     }
 
     // Data Structures
-    tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:data];   
-    tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:data];  
-    tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:data];  
-    array = [[NSMutableArray alloc] initWithArray:data];
-    dict = [NSMutableDictionary new]; 
-    for (id object in data) {
-        [dict setObject:object forKey:[object description]];
-    } 
-    
-    // Setup CoreData - simple Entity entry:value
-    NSEntityDescription *runEntity = [[NSEntityDescription alloc] init];
-    [runEntity setName:@"Entry"];
-    [runEntity setManagedObjectClassName:@"Entry"];
-     
-    NSAttributeDescription *attr= [[NSAttributeDescription alloc] init];
-    [attr setName:@"value"];
-    [attr setAttributeType:NSInteger32AttributeType];
-    [attr setOptional:false];
-    [attr setIndexed:true];
-    [attr setDefaultValue:@0];
-    [runEntity setProperties:@[attr]];
-    
-    // Managed Object Model
-    mom = [[NSManagedObjectModel alloc] init]; 
-    [mom setEntities:@[runEntity]]; 
-    
-    // Persistent Store - use in-memory
-    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
-    NSError *error = nil;
-    [psc addPersistentStoreWithType:NSInMemoryStoreType
-        configuration:nil URL:nil options:nil error:&error];
-    if (error) {
-        NSLog(@"Error creating persistent store: %@", error);
+    if (TREE) {
+        tree3 = [[NSTree alloc] initWithNodeCapacity:3 withSortedObjects:data];   
+        tree30 = [[NSTree alloc] initWithNodeCapacity:30 withSortedObjects:data];  
+        tree300 = [[NSTree alloc] initWithNodeCapacity:300 withSortedObjects:data];  
+    }
+    if (ARRAY) {
+        array = [[NSMutableArray alloc] initWithArray:data];
+    }
+    if (DICT) {
+        dict = [NSMutableDictionary new]; 
+        for (id object in data) {
+            [dict setObject:object forKey:[object description]];
+        } 
     }
     
-    // Managed Object Context
-    moc = [[NSManagedObjectContext alloc] init];
-    moc.persistentStoreCoordinator = psc;
+    if (CORE) {
+        // Setup CoreData - simple Entity entry:value
+        NSEntityDescription *runEntity = [[NSEntityDescription alloc] init];
+        [runEntity setName:@"Entry"];
+        [runEntity setManagedObjectClassName:@"Entry"];
+         
+        NSAttributeDescription *attr= [[NSAttributeDescription alloc] init];
+        [attr setName:@"value"];
+        [attr setAttributeType:NSInteger32AttributeType];
+        [attr setOptional:false];
+        [attr setIndexed:true];
+        [attr setDefaultValue:@0];
+        [runEntity setProperties:@[attr]];
+        
+        // Managed Object Model
+        mom = [[NSManagedObjectModel alloc] init]; 
+        [mom setEntities:@[runEntity]]; 
+        
+        // Persistent Store - use in-memory
+        NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
+        NSError *error = nil;
+    //    [psc addPersistentStoreWithType:NSInMemoryStoreType
+    //        configuration:nil URL:nil options:nil error:&error]; 
+        NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent: @"NSTreeExampleFunctionBenchmarks.sqlite"];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        [psc addPersistentStoreWithType:NSSQLiteStoreType 
+            configuration:nil URL:url options:nil error:&error];
+        if (error) {
+            NSLog(@"Error creating persistent store: %@", error);
+        }
+        
+        // Managed Object Context
+        moc = [[NSManagedObjectContext alloc] init];
+        moc.persistentStoreCoordinator = psc;
 
-    NSManagedObject *mo;
-    for (id object in data) {
-        mo = [NSEntityDescription 
-            insertNewObjectForEntityForName:@"Entry" 
-            inManagedObjectContext:moc]; 
-        [mo setValue:object forKey:@"value"]; 
+        NSManagedObject *mo;
+        for (id object in data) {
+            mo = [NSEntityDescription 
+                insertNewObjectForEntityForName:@"Entry" 
+                inManagedObjectContext:moc]; 
+            [mo setValue:object forKey:@"value"]; 
+        }
+        error = nil;
+        if (![moc save:&error]) {
+            NSLog(@"Error populating core data: %@", error); 
+        }
+        
+        NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:@"Entry"]; 
+        error = nil;
+        NSArray *results = [moc executeFetchRequest:fetch error:&error]; 
+        if (error) {
+            NSLog(@"Fetching from Core Data Failed: %@", error); 
+        }
+        NSLog(@"Core Data Entry Count: %i", results.count); 
     }
-    error = nil;
-    if (![moc save:&error]) {
-        NSLog(@"Error populating core data: %@", error); 
-    }
-    
-    NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:@"Entry"]; 
-    error = nil;
-    NSArray *results = [moc executeFetchRequest:fetch error:&error]; 
-    if (error) {
-        NSLog(@"Fetching from Core Data Failed: %@", error); 
-    }
-    NSLog(@"Core Data Entry Count: %i", results.count); 
 }
 
 + (void)tearDown
@@ -125,24 +142,28 @@ static NSManagedObjectContext *moc;
 #pragma mark - Insertion
 
 - (void)testInsertTree3 {
+    if (!TREE) return;
     for (id object in insertCriteria) {
         [tree3 addObject:object];
     }
 }
 
 - (void)testInsertTree30 {
+    if (!TREE) return;
     for (id object in insertCriteria) {
         [tree30 addObject:object];
     }
 }
 
 - (void)testInsertTree300 {
+    if (!TREE) return;
     for (id object in insertCriteria) {
         [tree300 addObject:object];
     }
 }
 
 - (void)testInsertArray {
+    if (!ARRAY) return;
     for (id object in insertCriteria) {
         [array insertObject:object atIndex:[array indexOfObject:object inSortedRange:NSMakeRange(0, array.count - 1) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(id obj1, id obj2) {
             return [obj1 compare:obj2];
@@ -151,12 +172,14 @@ static NSManagedObjectContext *moc;
 }
 
 - (void)testInsertDict {
+    if (!DICT) return;
     for (id object in insertCriteria) {
         [dict setObject:object forKey:[object description]];
     }
 }
 
 - (void)testInsertCoreData {
+    if (!CORE) return;
     NSManagedObject *mo;
     for (id object in insertCriteria) {
         mo = [NSEntityDescription 
@@ -174,37 +197,54 @@ static NSManagedObjectContext *moc;
 #pragma mark - Deletion
 
 - (void)testDeleteTree3 {
+    if (!TREE) return;
     for (id object in deleteCriteria) {
         [tree3 removeObject:object];
     }
 }
 
 - (void)testDeleteTree30 {
+    if (!TREE) return;
     for (id object in deleteCriteria) {
         [tree30 removeObject:object];
     }
 }
 
 - (void)testDeleteTree300 {
+    if (!TREE) return;
     for (id object in deleteCriteria) {
         [tree300 removeObject:object];
     }
 }
 
+- (void)testDeleteArrayBulk {
+    if (!ARRAY) return;
+    [array removeObjectsInArray:deleteCriteria];
+}
+
 - (void)testDeleteArray {
+    if (!ARRAY) return;
     for (id object in deleteCriteria) {
         [array removeObject:object];
     }
 }
 
 - (void)testDeleteDict {
+    if (!DICT) return;
     for (id object in deleteCriteria) {
         [dict removeObjectForKey:[object description]];  
     }
 }
 
+- (void)testDeleteDictBulk {
+    if (!DICT) return;
+    [dict removeObjectsForKeys:deleteCriteria];
+    NSLog(@"Delete Bulk Dict Count: %i", dict.count);
+}
+
 // This is really slow if done with multiple deleteCriteria
 - (void)testDeleteCoreData {
+    if (!CORE) return;
     NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:@"Entry"]; 
     NSError *error; 
     NSArray *results;
@@ -237,30 +277,35 @@ static NSManagedObjectContext *moc;
 #pragma mark - Search
 
 - (void)testSearchTree3 {
+    if (!TREE) return;
     for (id object in searchCriteria) {
         [tree3 containsObject:object];
     }
 }
 
 - (void)testSearchTree30 {
+    if (!TREE) return;
     for (id object in searchCriteria) {
         [tree30 containsObject:object];
     }
 }
 
 - (void)testSearchTree300 {
+    if (!TREE) return;
     for (id object in searchCriteria) {
         [tree300 containsObject:object];
     }
 }
 
 - (void)testSearchArray {
+    if (!ARRAY) return;
     for (id object in searchCriteria) {
         [array containsObject:object];
     }
 }
 
 - (void)testSearchDict {
+    if (!DICT) return;
     for (id object in searchCriteria) {
         [dict objectForKey:[object description]];  
     }
@@ -268,6 +313,7 @@ static NSManagedObjectContext *moc;
 
 // Extremely slow when doing on a lot of items
 - (void)testSearchCoreData {
+    if (!CORE) return;
     NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:@"Entry"]; 
     NSError *error; 
     NSMutableArray *results = [NSMutableArray new];
